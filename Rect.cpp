f4d7863bd9249b2853ed7367b85d597055f6189b
mdn756 <maximenee00@gmail.com>
2024-09-16 11:14:57 -0700

    Rooms connect better without crossing corridors

--- Rect.cpp
+++ Rect.cpp
@@ -1,5 +1,6 @@
 #include "Rect.h"
 #include <cmath>
+#include "Math.h"
 #include <iostream>
 
 std::vector<int> Rect::center() const
@@ -9,12 +10,12 @@ std::vector<int> Rect::center() const
 
 bool Rect::makeRoom(std::vector<std::vector<Tile>>& grid)
 {
-    m_isConnected = true; 
-    room = Room( this->left, this->right, this->top, this->bottom );
+    m_isConnected = true;
+    room = Room(this->left, this->right, this->top, this->bottom);
     // If all checks pass, place the room (fill grid with Floor tiles)
     for (int i = room->left; i < room->right; i++) {
         for (int j = room->bottom; j < room->top; j++) {
-            grid[j][i].setType(Floor);  
+            grid[j][i].setType(Floor);
         }
     }
     return true;
@@ -22,7 +23,7 @@ bool Rect::makeRoom(std::vector<std::vector<Tile>>& grid)
 
 bool Rect::connectRooms(std::vector<std::vector<Tile>>& grid)
 {
-    if (!children) 
+    if (!children)
     {
         // if no children, we are in a base room. Pass. 
         return false;
@@ -30,23 +31,77 @@ bool Rect::connectRooms(std::vector<std::vector<Tile>>& grid)
 
     for (int i = 0; i < children->size(); i++)
     {
-        if (!(*children)[i].m_isConnected) 
+        if (!(*children)[i].m_isConnected)
         {
+
             (*children)[i].connectRooms(grid);
         }
     }
     // once we get here all of the children should be connected within themselves.
     // connect them to each other.
-    connectCoords((*children)[0].center(), (*children)[1].center(), grid);    
-    m_isConnected = true;
-    return true;
+    std::vector<std::vector<int>> points = pathfind(grid);      // return a point from each children on their edges
+    connectCoords(points[0], points[1], grid);                  // connect the two points
+    
 }
 
-void Rect::connectCoords(std::vector<int> p1, std::vector<int> p2, 
+std::vector<std::vector<int>> Rect::pathfind(std::vector<std::vector<Tile>>& grid)
+{
+    std::vector<int> p1 = (*children)[0].sampleFloor(grid);  // Starting point
+    std::vector<int> p2 = (*children)[1].sampleFloor(grid);  // Ending point
+
+    std::vector<int> start = p1;
+    std::vector<int> end = p2;
+
+    int x = start[0];
+    int y = start[1];
+
+    // Move horizontally from p1[0] to p2[0] on row p1[1]
+    do {
+        // Move toward p2[0]
+        x += (p2[0] > x) ? 1 : -1;
+
+        if (grid[p1[1]][x].getType() == Floor)
+        {
+            if ((*children)[0].pointIsIn(std::vector<int>{x, p1[1]}))
+            {
+                start = std::vector<int>{ x, p1[1] };
+            }
+            else if ((*children)[1].pointIsIn(std::vector<int>{x, p1[1]}))
+            {
+                end = std::vector<int>{ x, p1[1] };
+                return { start, end };
+            }
+        }
+    } while (x != p2[0]);
+
+    // Move vertically from p1[1] to p2[1] (now x is aligned with p2[0])
+    do {
+        // Move toward p2[1]
+        y += (p2[1] > y) ? 1 : -1;
+
+        if (grid[y][p2[0]].getType() == Floor)
+        {
+            if ((*children)[0].pointIsIn(std::vector<int>{p2[0], y}))
+            {
+                start = std::vector<int>{ p2[1], y };
+            }
+            else if ((*children)[1].pointIsIn(std::vector<int>{p2[0], y}))
+            {
+                end = std::vector<int>{ p2[0], y };
+                return { start, end };
+            }
+        }
+    } while (y != p2[1]);
+
+    std::cout << "This should not print\n";
+    return { start, end }; // Return values in case no valid end is found
+}
+
+void Rect::connectCoords(std::vector<int> p1, std::vector<int> p2,
     std::vector<std::vector<Tile>>& grid)
 {
     // Move horizontally from p1[0] to p2[0] on row p1[1]
-    if (p1[0] != p2[0]) {
+    if (p1[0] <= p2[0]) {
         int start_x = std::min(p1[0], p2[0]);
         int end_x = std::max(p1[0], p2[0]);
         for (int x = start_x; x <= end_x; ++x) {
@@ -57,7 +112,7 @@ void Rect::connectCoords(std::vector<int> p1, std::vector<int> p2,
     }
 
     // Move vertically from p1[1] to p2[1] on column p2[0]
-    if (p1[1] != p2[1]) {
+    if (p1[1] <= p2[1]) {
         int start_y = std::min(p1[1], p2[1]);
         int end_y = std::max(p1[1], p2[1]);
         for (int y = start_y; y <= end_y; ++y) {
@@ -67,6 +122,24 @@ void Rect::connectCoords(std::vector<int> p1, std::vector<int> p2,
         }
     }
 }
+    
+
+std::vector<int> Rect::sampleFloor(std::vector<std::vector<Tile>>& grid)
+{
+    bool isFloor{ false };
+    int x{};
+    int y{};
+    while (!isFloor)
+    {
+        x = getRandom(left+1, right-1); //column
+        y = getRandom(bottom+1, top-1); //row    
+        if (grid[y][x].getType() == Floor)
+        {
+            isFloor = true;
+        }
+    }
+    return std::vector<int>{x, y};
+}
 
 void Rect::addChild(Rect child) 
 {
@@ -74,4 +147,14 @@ void Rect::addChild(Rect child)
         children = std::vector<Rect>(); 
     }
     children->push_back(child); 
+}
+
+bool Rect::pointIsIn(std::vector<int> point) const
+{
+    if (point[0] >= left && point[0] <= right && point[1] >= bottom && point[1] <= top)
+    {
+        return true;
+    }
+    else
+        return false;
 }
--- Rect.h
+++ Rect.h
@@ -18,13 +18,16 @@ public:
     int width() const { return right - left; }
     int height() const { return top - bottom; }
     int area() const { return width() * height(); }
+    bool pointIsIn(std::vector<int> point) const;
     std::vector<int> center() const;
 
     bool makeRoom(std::vector<std::vector<Tile>>& grid);
     bool connectRooms(std::vector<std::vector<Tile>>& grid);
-    void connectCoords(std::vector<int> p1, std::vector<int> p2, 
-        std::vector<std::vector<Tile>>& grid);
+    std::vector<std::vector<int>> pathfind(std::vector<std::vector<Tile>>& grid);
+    void connectCoords(std::vector<int> p1, std::vector<int> p2, std::vector<std::vector<Tile>>& grid);
     void addChild(Rect child);
+    std::vector<int> sampleFloor(std::vector<std::vector<Tile>>& grid);
+    
 
 private:
     std::optional<std::vector<Rect>> children;
